5445e9e9e85b232b3d57b8666ed7ef880d7b1045
alias <alias@MESHESHA>
2022-12-18 12:16:34 -0800

    Got GenerateSphere working as well as GenerateCube

--- Chunk.cpp
+++ Chunk.cpp
@@ -8,9 +8,10 @@ Chunk::Chunk(int chunksize) {
 std::vector<Vertex> Chunk::GenerateCube()
 {
 	std::vector<Vertex> output;
-	for (int i = 0; i < CHUNKSIZE; i++){
-		for (int j = 0; j < CHUNKSIZE; j++){
-			for (int k = 0; k < CHUNKSIZE; k++){
+	const int halfCHUNKSIZE = CHUNKSIZE / 2;
+	for (int i = -halfCHUNKSIZE; i < halfCHUNKSIZE; i++){
+		for (int j = -halfCHUNKSIZE; j < halfCHUNKSIZE; j++){
+			for (int k = -halfCHUNKSIZE; k < halfCHUNKSIZE; k++){
 				for (int z = 0; z < cubeData.size(); z++)
 				{
 					glm::vec3 pos = cubeData[z].Position;  // extracting the data
@@ -26,6 +27,21 @@ std::vector<Vertex> Chunk::GenerateCube()
 std::vector<Vertex> Chunk::GenerateSphere()
 {
 	std::vector<Vertex> output;
-
+	const int halfCHUNKSIZE = CHUNKSIZE / 2;
+	for (int i = -halfCHUNKSIZE; i < halfCHUNKSIZE; i++){
+		for (int j = -halfCHUNKSIZE; j < halfCHUNKSIZE; j++){
+			for (int k = -halfCHUNKSIZE; k < halfCHUNKSIZE; k++) {
+				for (int k = -halfCHUNKSIZE; k < halfCHUNKSIZE; k++) {
+					for (int z = 0; z < cubeData.size(); z++) {
+						glm::vec3 pos = cubeData[z].Position;
+						if (pow(pos.x + i, 2) + pow(pos.y + j, 2) + pow(pos.z + k, 2) <= pow(halfCHUNKSIZE, 2)) {
+							Vertex v(glm::vec3(pos.x + i, pos.y + j, pos.z + k));
+							output.push_back(v);
+						}
+					}
+				}
+			}
+		}
+	}
 	return output;
 }
